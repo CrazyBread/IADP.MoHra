0f292bdc257f6936a87203778003a9949c13b5d7
Евгений <iris94@inbox.ru>
2016-05-05 09:25:30 +0400

    SOME GOVNACODE!!!11!!1!

--- IADP.MoHra/IADP.MoHra/UI/MainForm.Designer.cs
+++ IADP.MoHra/IADP.MoHra/UI/MainForm.Designer.cs
@@ -28,20 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.spentTimeGridView = new System.Windows.Forms.DataGridView();
+            this.spentToEstimateGridView = new System.Windows.Forms.DataGridView();
+            this.onFixGridView = new System.Windows.Forms.DataGridView();
+            this.outOfHoursGridView = new System.Windows.Forms.DataGridView();
+            this.estimateTimeByRevisionGridView = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.spentTimeGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spentToEstimateGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onFixGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outOfHoursGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.estimateTimeByRevisionGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // spentTimeGridView
+            // 
+            this.spentTimeGridView.AllowUserToAddRows = false;
+            this.spentTimeGridView.AllowUserToDeleteRows = false;
+            this.spentTimeGridView.AllowUserToOrderColumns = true;
+            this.spentTimeGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.spentTimeGridView.Location = new System.Drawing.Point(6, 19);
+            this.spentTimeGridView.Name = "spentTimeGridView";
+            this.spentTimeGridView.ReadOnly = true;
+            this.spentTimeGridView.Size = new System.Drawing.Size(948, 288);
+            this.spentTimeGridView.TabIndex = 0;
+            // 
+            // spentToEstimateGridView
+            // 
+            this.spentToEstimateGridView.AllowUserToAddRows = false;
+            this.spentToEstimateGridView.AllowUserToDeleteRows = false;
+            this.spentToEstimateGridView.AllowUserToOrderColumns = true;
+            this.spentToEstimateGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.spentToEstimateGridView.Location = new System.Drawing.Point(6, 19);
+            this.spentToEstimateGridView.Name = "spentToEstimateGridView";
+            this.spentToEstimateGridView.ReadOnly = true;
+            this.spentToEstimateGridView.Size = new System.Drawing.Size(948, 288);
+            this.spentToEstimateGridView.TabIndex = 0;
+            // 
+            // onFixGridView
+            // 
+            this.onFixGridView.AllowUserToAddRows = false;
+            this.onFixGridView.AllowUserToDeleteRows = false;
+            this.onFixGridView.AllowUserToOrderColumns = true;
+            this.onFixGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.onFixGridView.Location = new System.Drawing.Point(6, 19);
+            this.onFixGridView.Name = "onFixGridView";
+            this.onFixGridView.ReadOnly = true;
+            this.onFixGridView.Size = new System.Drawing.Size(948, 288);
+            this.onFixGridView.TabIndex = 0;
+            // 
+            // outOfHoursGridView
+            // 
+            this.outOfHoursGridView.AllowUserToAddRows = false;
+            this.outOfHoursGridView.AllowUserToDeleteRows = false;
+            this.outOfHoursGridView.AllowUserToOrderColumns = true;
+            this.outOfHoursGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.outOfHoursGridView.Location = new System.Drawing.Point(6, 19);
+            this.outOfHoursGridView.Name = "outOfHoursGridView";
+            this.outOfHoursGridView.ReadOnly = true;
+            this.outOfHoursGridView.Size = new System.Drawing.Size(948, 288);
+            this.outOfHoursGridView.TabIndex = 1;
+            // 
+            // estimateTimeByRevisionGridView
+            // 
+            this.estimateTimeByRevisionGridView.AllowUserToAddRows = false;
+            this.estimateTimeByRevisionGridView.AllowUserToDeleteRows = false;
+            this.estimateTimeByRevisionGridView.AllowUserToOrderColumns = true;
+            this.estimateTimeByRevisionGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.estimateTimeByRevisionGridView.Location = new System.Drawing.Point(6, 18);
+            this.estimateTimeByRevisionGridView.Name = "estimateTimeByRevisionGridView";
+            this.estimateTimeByRevisionGridView.ReadOnly = true;
+            this.estimateTimeByRevisionGridView.Size = new System.Drawing.Size(948, 288);
+            this.estimateTimeByRevisionGridView.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.spentTimeGridView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(964, 318);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Оценка выполнения задач (S > 6; J < 2)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.spentToEstimateGridView);
+            this.groupBox2.Location = new System.Drawing.Point(12, 336);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(964, 318);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Отношение затраченного времни к оценке времени (S < 1; J > 1.5)";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.onFixGridView);
+            this.groupBox3.Location = new System.Drawing.Point(12, 660);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(964, 318);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = " Число задач, вернувшихся к доработке ко всем задачам (S < 0.1; J > 0.2)";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.outOfHoursGridView);
+            this.groupBox4.Location = new System.Drawing.Point(12, 984);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(964, 318);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = " Число смен статусов задач в нерабочее время (S > 0.15; J < 0.05)";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.estimateTimeByRevisionGridView);
+            this.groupBox5.Location = new System.Drawing.Point(12, 1308);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(964, 318);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Среднее число оценочных часов на одну ревизию (J < 2; S > 4)";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1004, 681);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.spentTimeGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spentToEstimateGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onFixGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outOfHoursGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.estimateTimeByRevisionGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView spentTimeGridView;
+        private System.Windows.Forms.DataGridView spentToEstimateGridView;
+        private System.Windows.Forms.DataGridView onFixGridView;
+        private System.Windows.Forms.DataGridView outOfHoursGridView;
+        private System.Windows.Forms.DataGridView estimateTimeByRevisionGridView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
 
--- IADP.MoHra/IADP.MoHra/UI/MainForm.cs
+++ IADP.MoHra/IADP.MoHra/UI/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Linq;
 
 namespace IADP.MoHra.UI
 {
@@ -7,6 +8,202 @@ namespace IADP.MoHra.UI
         public MainForm()
         {
             InitializeComponent();
+
+            #region dataGridsInit
+            var rep = new Model.Repository();
+
+            var dateStart = new System.DateTime(2015, 10, 1);
+
+            var spentTimeList = (
+                from total in rep.SpentTimeEvaluation(dateStart)
+                join oct in rep.SpentTimeEvaluation(dateStart, dateStart.AddMonths(1)) on total.MemberId equals oct.MemberId
+                into temp1
+                from oct in temp1.DefaultIfEmpty()
+                join nov in rep.SpentTimeEvaluation(dateStart.AddMonths(1), dateStart.AddMonths(2)) on total.MemberId equals nov.MemberId
+                into temp2
+                from nov in temp2.DefaultIfEmpty()
+                join dec in rep.SpentTimeEvaluation(dateStart.AddMonths(2), dateStart.AddMonths(3)) on total.MemberId equals dec.MemberId
+                into temp3
+                from dec in temp3.DefaultIfEmpty()
+                join jan in rep.SpentTimeEvaluation(dateStart.AddMonths(3), dateStart.AddMonths(4)) on total.MemberId equals jan.MemberId
+                into temp4
+                from jan in temp4.DefaultIfEmpty()
+                join feb in rep.SpentTimeEvaluation(dateStart.AddMonths(4), dateStart.AddMonths(5)) on total.MemberId equals feb.MemberId
+                into temp5
+                from feb in temp5.DefaultIfEmpty()
+                join mar in rep.SpentTimeEvaluation(dateStart.AddMonths(5), dateStart.AddMonths(6)) on total.MemberId equals mar.MemberId
+                into temp6
+                from mar in temp6.DefaultIfEmpty()
+                join apr in rep.SpentTimeEvaluation(dateStart.AddMonths(6), dateStart.AddMonths(7)) on total.MemberId equals apr.MemberId
+                into temp7
+                from apr in temp7.DefaultIfEmpty()
+                select new
+                {
+                    MemberName = total.MemberName
+                    , OctValue = oct?.Value
+                    , NovValue = nov?.Value
+                    , DecValue = dec?.Value
+                    , JanValue = jan?.Value
+                    , FebValue = feb?.Value
+                    , MarValue = mar?.Value
+                    , AprValue = apr?.Value
+                    , TotalValue = total.Value
+                }
+            ).ToList();
+            spentTimeGridView.DataSource = spentTimeList;
+
+            var spentToEstimateList = (
+                from total in rep.SpentToEstimateEvaluation(dateStart)
+                join oct in rep.SpentToEstimateEvaluation(dateStart, dateStart.AddMonths(1)) on total.MemberId equals oct.MemberId
+                into temp1
+                from oct in temp1.DefaultIfEmpty()
+                join nov in rep.SpentToEstimateEvaluation(dateStart.AddMonths(1), dateStart.AddMonths(2)) on total.MemberId equals nov.MemberId
+                into temp2
+                from nov in temp2.DefaultIfEmpty()
+                join dec in rep.SpentToEstimateEvaluation(dateStart.AddMonths(2), dateStart.AddMonths(3)) on total.MemberId equals dec.MemberId
+                into temp3
+                from dec in temp3.DefaultIfEmpty()
+                join jan in rep.SpentToEstimateEvaluation(dateStart.AddMonths(3), dateStart.AddMonths(4)) on total.MemberId equals jan.MemberId
+                into temp4
+                from jan in temp4.DefaultIfEmpty()
+                join feb in rep.SpentToEstimateEvaluation(dateStart.AddMonths(4), dateStart.AddMonths(5)) on total.MemberId equals feb.MemberId
+                into temp5
+                from feb in temp5.DefaultIfEmpty()
+                join mar in rep.SpentToEstimateEvaluation(dateStart.AddMonths(5), dateStart.AddMonths(6)) on total.MemberId equals mar.MemberId
+                into temp6
+                from mar in temp6.DefaultIfEmpty()
+                join apr in rep.SpentToEstimateEvaluation(dateStart.AddMonths(6), dateStart.AddMonths(7)) on total.MemberId equals apr.MemberId
+                into temp7
+                from apr in temp7.DefaultIfEmpty()
+                select new
+                {
+                    MemberName = total.MemberName
+                    , OctValue = oct?.Value
+                    , NovValue = nov?.Value
+                    , DecValue = dec?.Value
+                    , JanValue = jan?.Value
+                    , FebValue = feb?.Value
+                    , MarValue = mar?.Value
+                    , AprValue = apr?.Value
+                    , TotalValue = total.Value
+                }
+            ).ToList();
+            spentToEstimateGridView.DataSource = spentToEstimateList;
+
+            var onFixList = (
+                from total in rep.OnFixEvaluation(dateStart)
+                join oct in rep.OnFixEvaluation(dateStart, dateStart.AddMonths(1)) on total.MemberId equals oct.MemberId
+                into temp1
+                from oct in temp1.DefaultIfEmpty()
+                join nov in rep.OnFixEvaluation(dateStart.AddMonths(1), dateStart.AddMonths(2)) on total.MemberId equals nov.MemberId
+                into temp2
+                from nov in temp2.DefaultIfEmpty()
+                join dec in rep.OnFixEvaluation(dateStart.AddMonths(2), dateStart.AddMonths(3)) on total.MemberId equals dec.MemberId
+                into temp3
+                from dec in temp3.DefaultIfEmpty()
+                join jan in rep.OnFixEvaluation(dateStart.AddMonths(3), dateStart.AddMonths(4)) on total.MemberId equals jan.MemberId
+                into temp4
+                from jan in temp4.DefaultIfEmpty()
+                join feb in rep.OnFixEvaluation(dateStart.AddMonths(4), dateStart.AddMonths(5)) on total.MemberId equals feb.MemberId
+                into temp5
+                from feb in temp5.DefaultIfEmpty()
+                join mar in rep.OnFixEvaluation(dateStart.AddMonths(5), dateStart.AddMonths(6)) on total.MemberId equals mar.MemberId
+                into temp6
+                from mar in temp6.DefaultIfEmpty()
+                join apr in rep.OnFixEvaluation(dateStart.AddMonths(6), dateStart.AddMonths(7)) on total.MemberId equals apr.MemberId
+                into temp7
+                from apr in temp7.DefaultIfEmpty()
+                select new
+                {
+                    MemberName = total.MemberName
+                    , OctValue = oct?.Value
+                    , NovValue = nov?.Value
+                    , DecValue = dec?.Value
+                    , JanValue = jan?.Value
+                    , FebValue = feb?.Value
+                    , MarValue = mar?.Value
+                    , AprValue = apr?.Value
+                    , TotalValue = total.Value
+                }
+            ).ToList();
+            onFixGridView.DataSource = onFixList;
+
+            var outOfHoursList = (
+                from total in rep.OutOfHoursEvaluation(dateStart)
+                join oct in rep.OutOfHoursEvaluation(dateStart, dateStart.AddMonths(1)) on total.MemberId equals oct.MemberId
+                into temp1
+                from oct in temp1.DefaultIfEmpty()
+                join nov in rep.OutOfHoursEvaluation(dateStart.AddMonths(1), dateStart.AddMonths(2)) on total.MemberId equals nov.MemberId
+                into temp2
+                from nov in temp2.DefaultIfEmpty()
+                join dec in rep.OutOfHoursEvaluation(dateStart.AddMonths(2), dateStart.AddMonths(3)) on total.MemberId equals dec.MemberId
+                into temp3
+                from dec in temp3.DefaultIfEmpty()
+                join jan in rep.OutOfHoursEvaluation(dateStart.AddMonths(3), dateStart.AddMonths(4)) on total.MemberId equals jan.MemberId
+                into temp4
+                from jan in temp4.DefaultIfEmpty()
+                join feb in rep.OutOfHoursEvaluation(dateStart.AddMonths(4), dateStart.AddMonths(5)) on total.MemberId equals feb.MemberId
+                into temp5
+                from feb in temp5.DefaultIfEmpty()
+                join mar in rep.OutOfHoursEvaluation(dateStart.AddMonths(5), dateStart.AddMonths(6)) on total.MemberId equals mar.MemberId
+                into temp6
+                from mar in temp6.DefaultIfEmpty()
+                join apr in rep.OutOfHoursEvaluation(dateStart.AddMonths(6), dateStart.AddMonths(7)) on total.MemberId equals apr.MemberId
+                into temp7
+                from apr in temp7.DefaultIfEmpty()
+                select new
+                {
+                    MemberName = total.MemberName
+                    , OctValue = oct?.Value
+                    , NovValue = nov?.Value
+                    , DecValue = dec?.Value
+                    , JanValue = jan?.Value
+                    , FebValue = feb?.Value
+                    , MarValue = mar?.Value
+                    , AprValue = apr?.Value
+                    , TotalValue = total.Value
+                }
+            ).ToList();
+            outOfHoursGridView.DataSource = outOfHoursList;
+
+            var estimateTimeByRevisionList = (
+                from total in rep.EstimateTimeByRevisionEvaluation(dateStart)
+                join oct in rep.EstimateTimeByRevisionEvaluation(dateStart, dateStart.AddMonths(1)) on total.MemberId equals oct.MemberId
+                into temp1
+                from oct in temp1.DefaultIfEmpty()
+                join nov in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(1), dateStart.AddMonths(2)) on total.MemberId equals nov.MemberId
+                into temp2
+                from nov in temp2.DefaultIfEmpty()
+                join dec in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(2), dateStart.AddMonths(3)) on total.MemberId equals dec.MemberId
+                into temp3
+                from dec in temp3.DefaultIfEmpty()
+                join jan in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(3), dateStart.AddMonths(4)) on total.MemberId equals jan.MemberId
+                into temp4
+                from jan in temp4.DefaultIfEmpty()
+                join feb in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(4), dateStart.AddMonths(5)) on total.MemberId equals feb.MemberId
+                into temp5
+                from feb in temp5.DefaultIfEmpty()
+                join mar in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(5), dateStart.AddMonths(6)) on total.MemberId equals mar.MemberId
+                into temp6
+                from mar in temp6.DefaultIfEmpty()
+                join apr in rep.EstimateTimeByRevisionEvaluation(dateStart.AddMonths(6), dateStart.AddMonths(7)) on total.MemberId equals apr.MemberId
+                into temp7
+                from apr in temp7.DefaultIfEmpty()
+                select new
+                {
+                    MemberName = total.MemberName
+                    , OctValue = oct?.Value
+                    , NovValue = nov?.Value
+                    , DecValue = dec?.Value
+                    , JanValue = jan?.Value
+                    , FebValue = feb?.Value
+                    , MarValue = mar?.Value
+                    , AprValue = apr?.Value
+                    , TotalValue = total.Value
+                }
+            ).ToList();
+            estimateTimeByRevisionGridView.DataSource = estimateTimeByRevisionList;
+            #endregion
         }
     }
 }
